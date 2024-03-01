91902385c0656f974be4d9eb008b7c4fe52712fd
kibriarobin97 <kibriarobin97@gmail.com>
2024-03-01 16:50:15 +0600

    added card

--- js/tube.js
+++ js/tube.js
@@ -1,5 +1,6 @@
 const btnContainer = document.getElementById('btn-container');
-const cardContainer =document.getElementById('card-container');
+const cardContainer = document.getElementById('card-container');
+let displayCategory = 1000;
 
 const loadCategories = async () => {
     const res = await fetch('https://openapi.programming-hero.com/api/videos/categories');
@@ -17,15 +18,46 @@ const loadCategories = async () => {
 
 const loadDataByCategories = async (categoryId) => {
     // console.log(categoryId);
+    displayCategory = categoryId;
     const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/${categoryId}`);
     const data = await res.json();
     // console.log(data);
+    cardContainer.innerHTML = '';
     data.data.forEach(video => {
-        // console.log(video);
+        console.log(video);
+        let verifiedBadge = '';
+        if(video.authors[0].verified){
+            verifiedBadge =`<img class="w-6 h-6" src="./images/verified.png" alt="">`;
+        }
         const newCard = document.createElement('div');
+        newCard.innerHTML = `
+            <div class="card w-full bg-base-100 shadow-xl">
+                <figure class="overflow-hidden h-72 rounded-lg">
+                    <img class="w-4/5 min-h-52 rounded-lg" src="${video.thumbnail}" alt="" />
+                    <h6 class="absolute bottom-[40%] right-12">0 hr</h6>
+                </figure>
+                <div class="card-body">
+                    <div class="flex space-x-4 justify-start items-start">
+                        <div>
+                            <img class="w-12 h-12 rounded-full" src="${video.authors[0].profile_picture}" alt="" />
+                        </div>
+                        <div>
+                            <h2 class="card-title">${video.title}</h2>
+                            <div class="flex mt-3">
+                                <p class="">${video.authors[0].profile_name}</p>
+                                ${verifiedBadge}
+                            </div>
+                            <p class="mt-3">${video.others.views}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `;
+        cardContainer.appendChild(newCard);
     })
 }
 
 
 
 loadCategories();
+loadDataByCategories(displayCategory);
